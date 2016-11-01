83f36d7b66f3d4ed152dd3f70912987a93105d27
Dmitry <tyunkov.dima@gmail.com>
2016-11-01 14:11:54 +0600

    Initial commit. Full first version of application.
    Signed-off-by: Dmitry <tyunkov.dima@gmail.com>

---
+++ Saver_Images_from_OmSTU_Lib/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Saver_Images_from_OmSTU_Lib
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxURL = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDownStartPage = new System.Windows.Forms.NumericUpDown();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.buttonSelectPath = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDownEndPage = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownStartPage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownEndPage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL of book";
+            // 
+            // textBoxURL
+            // 
+            this.textBoxURL.Location = new System.Drawing.Point(107, 6);
+            this.textBoxURL.Name = "textBoxURL";
+            this.textBoxURL.Size = new System.Drawing.Size(198, 20);
+            this.textBoxURL.TabIndex = 1;
+            this.textBoxURL.Enter += new System.EventHandler(selectTextBoxText);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Start page";
+            // 
+            // numericUpDownStartPage
+            // 
+            this.numericUpDownStartPage.Location = new System.Drawing.Point(107, 32);
+            this.numericUpDownStartPage.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDownStartPage.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownStartPage.Name = "numericUpDownStartPage";
+            this.numericUpDownStartPage.Size = new System.Drawing.Size(66, 20);
+            this.numericUpDownStartPage.TabIndex = 2;
+            this.numericUpDownStartPage.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownStartPage.Enter += new System.EventHandler(selectNumUpDownText);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(125, 88);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 6;
+            this.buttonSave.Text = "Save book";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Path for save";
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(107, 59);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(106, 20);
+            this.textBoxPath.TabIndex = 4;
+            this.textBoxPath.Enter += new System.EventHandler(selectTextBoxText);
+            // 
+            // buttonSelectPath
+            // 
+            this.buttonSelectPath.Location = new System.Drawing.Point(219, 56);
+            this.buttonSelectPath.Name = "buttonSelectPath";
+            this.buttonSelectPath.Size = new System.Drawing.Size(86, 23);
+            this.buttonSelectPath.TabIndex = 5;
+            this.buttonSelectPath.Text = "Select path...";
+            this.buttonSelectPath.UseVisualStyleBackColor = true;
+            this.buttonSelectPath.Click += new System.EventHandler(this.buttonSelectPath_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(179, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "End page";
+            // 
+            // numericUpDownEndPage
+            // 
+            this.numericUpDownEndPage.Location = new System.Drawing.Point(238, 32);
+            this.numericUpDownEndPage.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDownEndPage.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownEndPage.Name = "numericUpDownEndPage";
+            this.numericUpDownEndPage.Size = new System.Drawing.Size(67, 20);
+            this.numericUpDownEndPage.TabIndex = 3;
+            this.numericUpDownEndPage.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownEndPage.Enter += new System.EventHandler(selectNumUpDownText);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 123);
+            this.Controls.Add(this.numericUpDownEndPage);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buttonSelectPath);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.numericUpDownStartPage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxURL);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Saver";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownStartPage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownEndPage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxURL;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDownStartPage;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.Button buttonSelectPath;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDownEndPage;
+    }
+}
+
---
+++ Saver_Images_from_OmSTU_Lib/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Saver_Images_from_OmSTU_Lib.Properties;
+
+namespace Saver_Images_from_OmSTU_Lib
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            string tempUriString = textBoxURL.Text;
+            string pathString = textBoxPath.Text;
+            if (!String.IsNullOrWhiteSpace(tempUriString))
+            {
+                if (!String.IsNullOrWhiteSpace(pathString))
+                {
+                    if (numericUpDownStartPage.Value <= numericUpDownEndPage.Value)
+                    {
+                        string uriString = tempUriString.Replace("id=ShowFT&o=sc", "id=FTShowPagePng");
+                        uriString = tempUriString.Replace("id=ShowFT&o=sp", "id=FTShowPagePng");
+                        for (var i = numericUpDownStartPage.Value; i <= numericUpDownEndPage.Value; i++)
+                        {
+                            string pageUriString = uriString + "&p=" + i;
+                            string fileString = pathString + Path.DirectorySeparatorChar + i + ".png";
+                            DownloadRemoteImageFile(pageUriString, fileString);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show(Resources.Form1_buttonSave_Click_Start_page_should_be_less_than_or_equal_to_end_page, Resources.Form1_buttonSave_Click_Error_page_number, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show(Resources.Form1_buttonSave_Click_Please_enter_path_and_try_again,
+                        Resources.Form1_buttonSave_Click_Empty_path, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show(Resources.Form1_buttonSave_Click_Please_enter_the_URL_and_try_again,
+                    Resources.Form1_buttonSave_Click_Empty_URL, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonSelectPath_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBoxPath.Text = folderBrowserDialog.SelectedPath;
+            }
+            
+        }
+
+        private static void DownloadRemoteImageFile(string uri, string fileName)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest) WebRequest.Create(uri);
+                HttpWebResponse response = (HttpWebResponse) request.GetResponse();
+
+                // Check that the remote file was found. The ContentType
+                // check is performed since a request for a non-existent
+                // image file might be redirected to a 404-page, which would
+                // yield the StatusCode "OK", even though the image was not
+                // found.
+                if ((response.StatusCode == HttpStatusCode.OK ||
+                     response.StatusCode == HttpStatusCode.Moved ||
+                     response.StatusCode == HttpStatusCode.Redirect) &&
+                    response.ContentType.StartsWith("image", StringComparison.OrdinalIgnoreCase))
+                {
+
+                    // if the remote file was found, download oit
+                    using (Stream inputStream = response.GetResponseStream())
+                    using (Stream outputStream = File.OpenWrite(fileName))
+                    {
+                        byte[] buffer = new byte[4096];
+                        int bytesRead;
+                        do
+                        {
+                            bytesRead = inputStream.Read(buffer, 0, buffer.Length);
+                            outputStream.Write(buffer, 0, bytesRead);
+                        } while (bytesRead != 0);
+                    }
+                }
+            }
+            catch (WebException exception)
+            {
+                if (exception.Status.Equals(WebExceptionStatus.ProtocolError))
+                {
+                    Thread.Sleep(TimeSpan.FromSeconds(10));
+                    DownloadRemoteImageFile(uri, fileName);
+                }
+                else
+                {
+                    MessageBox.Show(exception.Message, exception.Status.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Environment.Exit(1);
+                }
+            }
+        }
+
+        private void selectNumUpDownText(object numUpDownObj, EventArgs e)
+        {
+            var numericUpDown = numUpDownObj as NumericUpDown;
+            if (numericUpDown != null)
+                numericUpDown.Select(0, numericUpDown.Text.Length);
+        }
+
+        private void selectTextBoxText(object textBoxObj, EventArgs e)
+        {
+            var textBox = textBoxObj as TextBox;
+            if (textBox != null)
+                textBox.SelectAll();
+        }
+    }
+}
